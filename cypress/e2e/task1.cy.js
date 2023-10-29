74732e3f68d6e3ed96f3c8d809072be473496c3c
SGruba <salegrubazz@gmail.com>
2023-10-29 12:58:08 +0100

    Added task 2

--- cypress/e2e/task1.cy.js
+++ cypress/e2e/task1.cy.js
@@ -5,15 +5,12 @@ returned, or the list contains zero users. */
 describe('User list API test', () => {
     it('Should retrieve a list of users, failed if no response or empty list', () => {
       // request command makes an HTTP GET request to the specified URL.
-      cy.request('GET', 'https://gorest.co.in/public-api/users')
+      cy.request('GET', 'https://gorest.co.in/public/v2/users')
         .should((response) => {
-          // Inside this `.should` block, we perform assertions on the HTTP response.
-          
-          // Check that the HTTP status code is 200.
-          expect(response.status).to.eq(200);
-          
-          // Check that the response body contains a non-empty `data` property, which represents the list of users.
-          expect(response.body.data).to.not.be.empty;
+            expect(response.status).to.eq(200); // Check that the HTTP status code is 200.
+            
+            // Check that the response body contains a non-empty `data` property, which represents the list of users.
+            expect(response.body).to.not.be.empty;
         });
     });
 });
