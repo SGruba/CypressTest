3cc6217996c2c6bf569d967b90373fe5c0b7a02f
SGruba <salegrubazz@gmail.com>
2023-10-30 11:03:27 +0100

    Removed redundant comments. Ready for use.

--- cypress/e2e/ApiTesting.cy.js
+++ cypress/e2e/ApiTesting.cy.js
@@ -6,16 +6,15 @@ describe('User list API testing', () => {
     let users;
 
     beforeEach(() => {
-        // Before each test, make the GET request and store the response in the users variable.
         cy.request('GET', userUrl).then((response) => {
-            expect(response.status).to.eq(200); // Check that the HTTP status code is 200.
+            expect(response.status).to.eq(200);
             users = response.body;
         });
     });
 
     // TEST 1:
     it('Should retrieve a list of users, failed if no response or empty list', () => {
-        // Check that list of users is not empty.
+
         expect(users).to.not.be.empty;
     });
 
@@ -28,13 +27,12 @@ describe('User list API testing', () => {
         const userWithC = users.find((user) => user.name.startsWith('C'));
         */
 
-        // This will check every word in Name, so that titles don't intervene with the check.
-        const wordStartsWithC = (str) => {
+        const anyWordStartsWithC = (str) => {
             const words = str.split(' ');
             return words.some(word => /^C/i.test(word));
         };
         // Check if any word in the name starts with 'C'
-        const userWithC = users.find((user) => wordStartsWithC(user.name));
+        const userWithC = users.find((user) => anyWordStartsWithC(user.name));
                 
         if (userWithC) {
           console.log("At least one user with a starting 'C' in their name, for example: ", userWithC);
@@ -42,7 +40,6 @@ describe('User list API testing', () => {
           console.log("No users with a name starting with 'C'");
         }
 
-        // Check that such a user exists.
         expect(userWithC).to.exist;
 
     });
@@ -54,10 +51,9 @@ describe('User list API testing', () => {
     
         console.log('List of Users:');
       
-        // Printing out the list in the browser console
         console.log(users)
 
-        // Printing out the list, per-user, in the browser console
+        // Additionally printing out the list, per-user, in the browser console
         var num = 1;
         console.log('List of Users:');
         users.forEach((user) => {
