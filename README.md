045171a0b644f617c45e2bb45529425be0286e4b
SGruba <salegrubazz@gmail.com>
2023-10-30 10:27:02 +0100

    Added Readme and refactored ApiTesting.

---
+++ README.md
@@ -0,0 +1,7 @@
+QA - Analyst tech challenge
+
+This is a project that covers testing of retrieval of user data via REST API
+from https://gorest.co.in. This project also covers a user story for buying
+an item from https://www.saucedemo.com/.
+
+This project is done in Cypress v13.3.3.
--- cypress/e2e/ApiTesting.cy.js
+++ cypress/e2e/ApiTesting.cy.js
@@ -3,81 +3,70 @@
 describe('User list API testing', () => {
     
     const userUrl = Cypress.env('userUrl');
+    let users; // A variable to store the users for all tests.
+
+    beforeEach(() => {
+        // Before each test, make the GET request and store the response in the users variable.
+        cy.request('GET', userUrl).then((response) => {
+            expect(response.status).to.eq(200); // Check that the HTTP status code is 200.
+            users = response.body;
+        });
+    });
 
     // TEST 1: Retrieve a list of users and verify that at least one user is being returned.
     it('Should retrieve a list of users, failed if no response or empty list', () => {
-
-        cy.request('GET', userUrl)
-          .should((response) => {
-            
-            expect(response.status).to.eq(200); // Check that the HTTP status code is 200.
-            
-            // Check that the response body, which represents the list of users, is not empty.
-            expect(response.body).to.not.be.empty;
-        });
+        // Check that list of users is not empty.
+        expect(users).to.not.be.empty;
     });
 
     // TEST 2: Retrieve a list of users and verify that there is at least one user whose name starts with 'C'.
     it('Should retrieve a list of users and check if a user starts with C, failed if no response or no C user', () => {
 
-        cy.request('GET', userUrl)
-          .should((response) => {
-            
-            expect(response.status).to.eq(200); // Check HTTP response
-            const users = response.body; // Extract the list of users from the response.
-
-            // Check that list is not empty.
-            expect(users).to.not.be.empty;
+        expect(users).to.not.be.empty;
   
-            /* This is a basic version of search where only first symbol is getting checked:
-            const userWithC = users.find((user) => user.name.startsWith('C'));
-            */
+        /* Commented below is a basic version of search where only first symbol is getting checked:
+        const userWithC = users.find((user) => user.name.startsWith('C'));
+        */
 
-            /* This is a more advanced version where each word is checked. 
-            Now, I am aware this system might misfire on a title or something else, but it
-            should catch any names that are written after titles.
-            */
-            const wordStartsWithC = (str) => {
-                const words = str.split(' ');
-                return words.some(word => /^C/i.test(word));
-            };
-            // Check if any word in the name starts with 'C'
-            const userWithC = users.find((user) => wordStartsWithC(user.name));
-                    
-            if (userWithC) {
-              console.log("At least one user with a starting 'C' in their name, for example: ", userWithC);
-            } else {
-              console.log("No users with a name starting with 'C'");
-            }
+        /* This is a more advanced version where each word is checked. 
+        Now, I am aware this system might misfire on a title or something else, but it
+        should catch any names that are written after titles.
+        */
+        const wordStartsWithC = (str) => {
+            const words = str.split(' ');
+            return words.some(word => /^C/i.test(word));
+        };
+        // Check if any word in the name starts with 'C'
+        const userWithC = users.find((user) => wordStartsWithC(user.name));
+                
+        if (userWithC) {
+          console.log("At least one user with a starting 'C' in their name, for example: ", userWithC);
+        } else {
+          console.log("No users with a name starting with 'C'");
+        }
 
-            // Check that such a user exists.
-            expect(userWithC).to.exist;
-          });
+        // Check that such a user exists.
+        expect(userWithC).to.exist;
+
     });
 
     // TEST 3: Retrieve a list of users and display it in the console.
     it('Should retrieve a list of users, than print it out in console. Failed if no response or empty list', () => {
 
-        cy.request('GET', userUrl)
-          .should((response) => {
-             
-            expect(response.status).to.eq(200); // Check HTTP response
-            const users = response.body; // Extract the list of users from the response.
+        // Check that list is not empty.
+        expect(users).to.not.be.empty;
+    
+        console.log('List of Users:');
       
-            console.log('List of Users:');
-      
-            // Check that list is not empty.
-            expect(users).to.not.be.empty;
-      
-            // Printing out the list in the browser console
-            console.log(users)
+        // Printing out the list in the browser console
+        console.log(users)
 
-            // Printing out the list, per-user, in the browser console
-            var num = 1;
-            console.log('List of Users:');
-            users.forEach((user) => {
-              console.log(num++, user.name);
-            });
-          });
+        // Printing out the list, per-user, in the browser console
+        var num = 1;
+        console.log('List of Users:');
+        users.forEach((user) => {
+          console.log(num++, user.name);
+        });
+
     });
 });
